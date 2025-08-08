6f6dcbf12a2da3b98c00044381471b8800acdd7e
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-09 01:41:27 +0200

    Add files via upload

---
+++ qelyos_ecom_dynamic_picking_final/models/res_config_settings.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    ecom_dynamic_source_location_ids = fields.Many2many(
+        comodel_name="stock.location",
+        related="company_id.ecom_dynamic_source_location_ids",
+        readonly=False
+    )
+
+    ecom_dynamic_strategy = fields.Selection(
+        selection=[
+            ("first_available", "Premier emplacement avec stock"),
+            ("default_only", "Toujours dépôt par défaut"),
+        ],
+        related="company_id.ecom_dynamic_strategy",
+        readonly=False
+    )
+
+    ecom_dynamic_website_id = fields.Many2one(
+        comodel_name="website",
+        related="company_id.ecom_dynamic_website_id",
+        readonly=False
+    )
