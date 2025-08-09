92f8096b04d51d22e271e36af56bcd6dd8c4d4ee
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-09 02:30:39 +0200

    Add files via upload

---
+++ qelyos_ecom_dynamic_picking_auto_patch/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "eCommerce – Sélection dynamique avec réassort automatique (patch)",
+    "summary": "Choix automatique de la boutique expéditrice pour eCommerce + réassort interne si rupture + mise à jour moves",
+    "version": "18.0.1.0.1",
+    "author": "Qelyos",
+    "website": "https://qelyos.com",
+    "depends": ["sale", "stock", "website_sale"],
+    "data": [
+        "views/res_config_settings_views.xml"
+    ],
+    "application": False,
+    "license": "LGPL-3"
+}
