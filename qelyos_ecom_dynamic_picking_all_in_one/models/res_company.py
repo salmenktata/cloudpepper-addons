694897020124971bdd58560561280f897bb7b408
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-11 21:17:39 +0200

    Add files via upload

---
+++ qelyos_ecom_dynamic_picking_all_in_one/models/res_company.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    ecom_dynamic_source_location_ids = fields.Many2many(
+        "stock.location",
+        "res_company_ecom_loc_rel",
+        "company_id",
+        "location_id",
+        string="Emplacements sources possibles",
+        help="Inclure CENTRAL/Stock puis les boutiques (ex: Gafsa/Stock, Sousse/Stock, Tunis/Stock)."
+    )
+
+    ecom_dynamic_strategy = fields.Selection(
+        selection=[
+            ("first_available", "Premier emplacement avec stock"),
+            ("most_available", "Emplacement avec plus de stock (disponible)"),
+            ("central_then_maxstock_then_order", "CENTRAL > Max stock (Gafsa/Sousse/Tunis) > Ordre fixe (Gafsa>Sousse>Tunis)"),
+            ("default_only", "Toujours dépôt par défaut"),
+        ],
+        string="Stratégie de sélection",
+        default="central_then_maxstock_then_order",
+        help="Règle de sélection de l'emplacement source lors de la création des pickings eCommerce."
+    )
+
+    ecom_dynamic_website_id = fields.Many2one(
+        "website",
+        string="Limiter à un site web",
+        help="Si renseigné, la logique ne s'applique qu'à ce site web (eCommerce)."
+    )
