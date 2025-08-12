d6db110af27e81551351ee17ace9ee9876e94026
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-12 03:05:32 +0200

    Add files via upload

---
+++ qelyos_ecom_dynamic_picking_all_steps_v3_available/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "eCommerce – Sélection dynamique (1/2/3 étapes) + Réassort + Forçage (v3 CENTRAL available)",
+    "summary": "CENTRAL prioritaire calculé en quantité disponible (réservations exclues). Support 1/2/3 étapes, mise à jour De/Vers, réassort.",
+    "version": "18.0.6.2.0",
+    "author": "Qelyos",
+    "website": "https://qelyos.com",
+    "depends": ["sale", "stock", "website_sale", "sale_management"],
+    "data": ["views/res_config_settings_views.xml"],
+    "application": False,
+    "license": "LGPL-3"
+}
