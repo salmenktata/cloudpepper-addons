e96eb06cdcea9a804ce59427c5535b9983f3f844
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-12 04:09:09 +0200

    Delete qelyos_ecom_dynamic_picking_all_steps_v8_2_plain_log_dual directory

--- qelyos_ecom_dynamic_picking_all_steps_v8_2_plain_log_dual/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Qelyos eCom Dynamic Picking All Steps V8.2 Plain Log Dual',
-    'version': '8.2',
-    'category': 'Inventory',
-    'summary': 'Dynamic picking with logs in both picking and sales order chatter',
-    'depends': ['sale_management', 'stock'],
-    'data': [],
-    'installable': True,
-}
