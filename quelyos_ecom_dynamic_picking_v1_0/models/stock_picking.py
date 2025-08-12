a662ac8b69f9287e176db1348e8847cb887df9c2
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-12 16:23:33 +0200

    Update stock_picking.py

--- quelyos_ecom_dynamic_picking_v1_0/models/stock_picking.py
+++ quelyos_ecom_dynamic_picking_v1_0/models/stock_picking.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    quelyos_log = fields.Text(string="Quelyos Dual Log (local)")
+    quelyos_log = fields.Text(string=_("Quelyos Dual Log (local)"))
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -16,6 +16,15 @@ class StockPicking(models.Model):
                     "origin": rec.origin, "picking_type": rec.picking_type_id.display_name,
                 })
         return records
+
+    def write(self, vals):
+        # Add server-side validation for source location
+        if 'location_id' in vals:
+            allowed_ids_str = self.env["ir.config_parameter"].sudo().get_param("quelyos_ecom_dynamic_picking.allowed_src_ids") or ""
+            allowed_ids = [int(x) for x in allowed_ids_str.split(",") if x]
+            if allowed_ids and vals.get('location_id') and vals['location_id'] not in allowed_ids:
+                raise ValidationError(_("L'emplacement source sélectionné n'est pas autorisé."))
+        return super(StockPicking, self).write(vals)
 
     def action_assign(self):
         res = super().action_assign()
@@ -35,12 +44,17 @@ class StockPicking(models.Model):
         return res
 
     def _quelyos_dual_log(self, event, extra=None):
+        """ Log an event and post a message on the picking. """
         msg = "[QUELYOS][%s] %s" % (event.upper(), extra or {})
         for rec in self:
             rec.quelyos_log = (rec.quelyos_log or "") + (("\n" if rec.quelyos_log else "") + msg)
             rec.message_post(body=msg)
 
     def _quelyos_check_strict_order_before_validate(self):
+        """
+        Checks if other related pickings in the same group or with the same origin
+        need to be validated first, based on picking type sequence.
+        """
         self.ensure_one()
         if not self.group_id and not self.origin:
             return
