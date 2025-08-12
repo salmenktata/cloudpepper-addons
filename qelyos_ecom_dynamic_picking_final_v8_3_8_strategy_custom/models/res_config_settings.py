6965792cd0b0a760d4dfb23f764f29a2df6f7c15
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-12 14:25:12 +0200

    Update res_config_settings.py

--- qelyos_ecom_dynamic_picking_final_v8_3_8_strategy_custom/models/res_config_settings.py
+++ qelyos_ecom_dynamic_picking_final_v8_3_8_strategy_custom/models/res_config_settings.py
@@ -1,13 +1,27 @@
-# -*- coding: utf-8 -*-
 from odoo import fields, models
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
+    _inherit = 'res.config.settings'
 
-    ecom_dynamic_enabled = fields.Boolean(related="company_id.ecom_dynamic_enabled", readonly=False)
-    ecom_dynamic_only_website = fields.Boolean(related="company_id.ecom_dynamic_only_website", readonly=False)
-    ecom_dynamic_strict_order = fields.Boolean(related="company_id.ecom_dynamic_strict_order", readonly=False)
-    ecom_dynamic_dual_log = fields.Boolean(related="company_id.ecom_dynamic_dual_log", readonly=False)
-    ecom_dynamic_pick_steps = fields.Selection(related="company_id.ecom_dynamic_pick_steps", readonly=False)
-    ecom_dynamic_out_steps = fields.Selection(related="company_id.ecom_dynamic_out_steps", readonly=False)
-    ecom_dynamic_source_location_ids = fields.Many2many(related="company_id.ecom_dynamic_source_location_ids", readonly=False)
+    ecom_dynamic_enabled = fields.Boolean("Enable Dynamic Picking Strategy")
+    ecom_dynamic_strategy = fields.Selection([
+        ('qelyos_custom', 'Qelyos Custom Strategy'),
+        ('standard', 'Odoo Standard'),
+    ], string="Dynamic Picking Strategy", default='qelyos_custom')
+    ecom_dynamic_free_stock_only = fields.Boolean("Use only free stock (exclude reserved)", default=True)
+
+    def set_values(self):
+        res = super().set_values()
+        self.env['ir.config_parameter'].sudo().set_param('ecom_dynamic_enabled', self.ecom_dynamic_enabled)
+        self.env['ir.config_parameter'].sudo().set_param('ecom_dynamic_strategy', self.ecom_dynamic_strategy)
+        self.env['ir.config_parameter'].sudo().set_param('ecom_dynamic_free_stock_only', self.ecom_dynamic_free_stock_only)
+        return res
+
+    def get_values(self):
+        res = super().get_values()
+        res.update(
+            ecom_dynamic_enabled=self.env['ir.config_parameter'].sudo().get_param('ecom_dynamic_enabled', 'False') == 'True',
+            ecom_dynamic_strategy=self.env['ir.config_parameter'].sudo().get_param('ecom_dynamic_strategy', 'qelyos_custom'),
+            ecom_dynamic_free_stock_only=self.env['ir.config_parameter'].sudo().get_param('ecom_dynamic_free_stock_only', 'True') == 'True'
+        )
+        return res
