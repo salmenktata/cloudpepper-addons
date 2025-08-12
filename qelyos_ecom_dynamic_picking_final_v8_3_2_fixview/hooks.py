3fb02750ef745ac4442c555cc5f26ab5a430ad59
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-12 04:55:31 +0200

    Delete qelyos_ecom_dynamic_picking_final_v8_3_2_fixview directory

--- qelyos_ecom_dynamic_picking_final_v8_3_2_fixview/hooks.py
+++
@@ -1,5 +0,0 @@
-def post_init_hook(cr, registry):
-    cr.execute("""
-    CREATE INDEX IF NOT EXISTS idx_stock_move_picking_seq_state
-    ON stock_move (picking_id, sequence, state);
-    """)
--- qelyos_ecom_dynamic_picking_final_v8_3_2_fixview/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import res_company
-from . import res_config_settings
-from . import stock_move
-from . import stock_picking
