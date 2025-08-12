95b30f84c2b4d8c314ccf236e417ca7a0c950e1b
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-12 13:06:49 +0200

    Add files via upload

---
+++ qelyos_ecom_dynamic_picking_final_v8_3_8_strategy_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Qelyos eCommerce Dynamic Picking (v8.3.8 Strategy Custom)",
+    "summary": "Implements custom picking source strategy (CENT first, then boutiques priority, else create replenishment to Soukra).",
+    "version": "18.0.8.3.8",
+    "author": "Qelyos",
+    "license": "LGPL-3",
+    "website": "https://qelyos.com",
+    "category": "Inventory/Customization",
+    "depends": ["stock", "sale_management", "website_sale", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_config_settings_views.xml",
+        "views/stock_picking_views.xml"
+    ],
+    "assets": {},
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
