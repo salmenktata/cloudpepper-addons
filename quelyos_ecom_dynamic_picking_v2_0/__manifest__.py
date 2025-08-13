e33b1301cc0bf60cec6af611346d245ab42b15bf
Salmen KTATA <salmen.ktata@gmail.com>
2025-08-13 13:58:53 +0100

    Add files via upload

---
+++ quelyos_ecom_dynamic_picking_v2_0/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quelyos - Dynamic Picking",
+    'version': '1.0',
+    'summary': "Sélection automatique de l'emplacement source pour les commandes eCommerce",
+    'description': """
+Ce module permet d'appliquer une stratégie dynamique pour déterminer automatiquement 
+l'emplacement source des livraisons selon des critères configurables :
+- Priorité au stock central si possible
+- Sinon meilleure boutique (stock libre max)
+- Réassort automatique si besoin
+    """,
+    'author': "Quelyos",
+    'website': "https://quelyos.com",
+    'category': 'Inventory/Inventory',
+    'depends': ['sale', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
