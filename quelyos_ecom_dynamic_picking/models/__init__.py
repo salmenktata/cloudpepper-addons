c2a9b34b154302165c3fbf2c6d821769b25fee67
Salmen KTATA <salmen.ktata@gmail.com>
2025-09-02 09:50:10 +0200

    Update __init__.py

--- quelyos_ecom_dynamic_picking/models/__init__.py
+++ quelyos_ecom_dynamic_picking/models/__init__.py
@@ -1,7 +1,4 @@
-*** a/models/__init__.py
---- b/models/__init__.py
-@@
- # -*- coding: utf-8 -*-
- from . import res_config_settings
- from . import stock_picking
-+from . import stock_move
+# -*- coding: utf-8 -*-
+from . import res_config_settings
+from . import stock_picking
+from . import stock_move
