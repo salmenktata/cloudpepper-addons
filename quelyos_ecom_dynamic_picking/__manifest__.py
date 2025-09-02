03a3c2d4d8374a22fbcdd6f1bbfbdd706c2d99ee
Salmen KTATA <salmen.ktata@gmail.com>
2025-09-02 09:08:34 +0200

    Update __manifest__.py

--- quelyos_ecom_dynamic_picking/__manifest__.py
+++ quelyos_ecom_dynamic_picking/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Quelyos – Dynamic Picking",
     "summary": "Sélection dynamique de l’emplacement source + réassort interne auto pour livraisons sortantes",
-    "version": "2.2.3",
+    "version": "2.3.0",
     "category": "Inventory/Delivery",
     "author": "Qelyos",
     "website": "https://quelyos.com",
